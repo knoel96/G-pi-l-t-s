2fad051508e6ff0c1057a93a15e4ad4526c2c533
knoel96 <45892742+knoel96@users.noreply.github.com>
2019-01-20 17:40:56 +0100

    Update jeltablfelism.py

--- jeltablfelism.py
+++ jeltablfelism.py
@@ -1,6 +1,4 @@
-Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from imutils.perspective import four_point_transform
+from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
 import cv2
